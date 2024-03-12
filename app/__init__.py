eff8c514c3a6a5db00d0ba70eb349c2d54956e6c
“calebdamian” <naranjocaleb@gmail.com>
2024-03-12 16:29:47 -0500

    Cambios en las credenciales de conexión a la base de datos.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(32)
 db = SQLAlchemy(app)
 
--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin123@127.0.0.1/pytestcdna'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
