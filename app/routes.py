8d188a77c7af95c2db32ff3b6e738e9d86451288
“calebdamian” <naranjocaleb@gmail.com>
2024-03-12 16:18:01 -0500

    Se añade búsqueda y se mejora la documentación además de la legibilidad del código.

--- app/routes.py
+++ app/routes.py
@@ -3,34 +3,51 @@ from app import app, db
 from app.models import Persona
 from app.forms import PersonaForm
 from flask_paginate import Pagination, get_page_args
+
+
 @app.route('/')
 def index():
-    """Ruta principal para mostrar todas las personas, además de las acciones disponibles."""
-      # Obtener los parámetros de la paginación desde la solicitud HTTP
+    """Main route which contains main functionality and all records."""
+    
+    # Obtener el término de búsqueda del parámetro de consulta
+    search_term = request.args.get('search_term', '')
+
+    # Filtrar las personas según el término de búsqueda
+    if search_term:
+        personas_query = Persona.query.filter(
+            Persona.nombre.ilike(f'%{search_term}%') | Persona.apellido.ilike(f'%{search_term}%')
+        )
+    else:
+        personas_query = Persona.query
+    
+    # Obtener los parámetros de la paginación desde la solicitud HTTP
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
 
-    # Obtener todas las personas de la base de datos
-    personas = Persona.query.order_by(Persona.id).offset(offset).limit(per_page).all()
+    # Obtener todas las personas de la base de datos con paginación
+    personas_paginadas = personas_query.order_by(Persona.id).offset(offset).limit(per_page).all()
 
     # Crear la instancia de paginación
-    pagination = Pagination(page=page, per_page=per_page, total=Persona.query.count())
+    pagination = Pagination(page=page, per_page=per_page, total=personas_query.count())
 
-    return render_template('index.html', personas=personas, pagination= pagination)
+    return render_template('index.html', personas=personas_paginadas, pagination=pagination, search_term=search_term)
 
 @app.route('/agregar', methods=['GET', 'POST'])
 def agregar():
-    """Ruta para agregar una nueva persona."""
+    """Route to add a new person.
+    Returns a form with the required fields."""
     form = PersonaForm(request.form)
     if request.method == 'POST' and form.validate():
         try:
-         nueva_persona = Persona(nombre=form.nombre.data, 
-                                apellido=form.apellido.data, 
-                                edad=form.edad.data, 
-                                direccion=form.direccion.data, 
-                                correo=form.correo.data)
-         db.session.add(nueva_persona)
-         db.session.commit()
-         return redirect(url_for('index'))
+            nueva_persona = Persona(
+                nombre=form.nombre.data, 
+                apellido=form.apellido.data, 
+                edad=form.edad.data, 
+                direccion=form.direccion.data, 
+                correo=form.correo.data
+            )
+            db.session.add(nueva_persona)
+            db.session.commit()
+            return redirect(url_for('index'))
         except Exception as e:
             return render_template('error.html', error=str(e)), 500
 
@@ -38,31 +55,32 @@ def agregar():
 
 @app.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar(id):
-    """Ruta para editar una persona existente."""
+    """Route to edit an existing person.
+    Returns a form with the required fields."""
     persona = Persona.query.get_or_404(id)
     if request.method == 'POST':
         try:
-         persona.nombre = request.form['nombre']
-         persona.apellido = request.form['apellido']
-         persona.edad = request.form['edad']
-         persona.direccion = request.form['direccion']
-         persona.correo = request.form['correo']
+            persona.nombre = request.form['nombre']
+            persona.apellido = request.form['apellido']
+            persona.edad = request.form['edad']
+            persona.direccion = request.form['direccion']
+            persona.correo = request.form['correo']
         
-         db.session.commit()
+            db.session.commit()
         
-         return redirect(url_for('index'))
+            return redirect(url_for('index'))
         except Exception as e:
-           return render_template('error.html', error=str(e)), 500
+            return render_template('error.html', error=str(e)), 500
         
     return render_template('editar_persona.html', persona=persona)
 
 @app.route('/eliminar/<int:id>')
 def eliminar(id):
-    """Ruta para eliminar una persona."""
+    """Route to delete a person."""
     try:
-     persona = Persona.query.get_or_404(id)
-     db.session.delete(persona)
-     db.session.commit()
-     return redirect(url_for('index'))
+        persona = Persona.query.get_or_404(id)
+        db.session.delete(persona)
+        db.session.commit()
+        return redirect(url_for('index'))
     except Exception as e:
         return render_template('error.html', error=str(e)), 500
